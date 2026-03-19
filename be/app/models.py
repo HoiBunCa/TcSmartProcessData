6b16952c1e1975169e18e908d2f5ef35a1fa15bb
dinhvanhung <boybka23@gmail.com>
2026-03-19 08:47:48 +0700

    update upload file theo session

--- be/app/models.py
+++ be/app/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+import os
+from datetime import datetime
 
+def upload_to_session(instance, filename):
+    now = datetime.now()
+    return os.path.join(
+        "uploads",
+        now.strftime("%Y/%m/%d"),
+        instance.session_id,
+        filename
+    )
 
 class File(models.Model):
     """Uploaded file stored in MEDIA_ROOT.
@@ -10,7 +20,7 @@ class File(models.Model):
     """
 
     session_id = models.CharField(max_length=64, db_index=True)
-    file = models.FileField(upload_to="uploads/%Y/%m/%d/")
+    file = models.FileField(upload_to=upload_to_session)
     original_name = models.CharField(max_length=255)
     size = models.BigIntegerField(default=0)
     content_type = models.CharField(max_length=100, blank=True, default="")
